bcf4087e900f00e52bd43b679277937aa4abf719
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-02 05:23:48 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,29 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
+   def initialize(name)
+    @status = "open"
+    @name = name
+    @balance = 1000
+    end
 
+    def deposit(number)
+      self.balance = self.balance + number
+    end
+    def display_balance
+      return "Your balance is $#{self.balance}."
+    end
+
+    def close_account
+      self.status = "closed"
+    end
+
+   def valid?
+      if self.status == "open" && self.balance >0
+        return true
+      else
+        return false
+      end
+   end
+   
 end
