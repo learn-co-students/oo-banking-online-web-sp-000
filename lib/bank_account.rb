cf3b47a18c4adff1cd35065a052cb0bac7fd3506
Jon Mackey <jon@jonmackey.party>
2018-12-11 21:53:47 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,37 @@
 class BankAccount
-
+  attr_accessor :balance, :status
+  attr_reader :name
+  @@all = [ ]
+  
+  def initialize(name)
+    @name = name 
+    @balance = 1000
+    @status = 'open'
+    @@all << self
+  end
+  
+  def deposit(amount)
+    @balance = @balance + amount 
+  end
+  
+  def withdraw(amount)
+    @balance = @balance - amount
+  end
+  
+  def display_balance
+    "Your balance is $#{@balance}."
+  end 
+  
+  def valid? 
+    if @balance > 0 && @status == 'open'
+      true 
+    else 
+      false
+    end
+  end 
+  
+  def close_account
+    @status = 'closed' 
+  end
+  
 end
