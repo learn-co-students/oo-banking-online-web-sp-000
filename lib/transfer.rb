0ef4ed7f704f3f788246559fbecda3b04e936def
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-17 06:10:57 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,20 +1,23 @@
 class Transfer
   attr_accessor :status
   attr_reader :sender, :receiver, :amount
+
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @amount = amount
     @status = "pending"
   end
-   def valid?
-     if self.sender.valid? && self.receiver.valid?
-       return true
-     else
-       false
-     end
+
+  def valid?
+    if self.sender.valid? && self.receiver.valid?
+      return true
+    else
+      false
+    end
   end
-   def execute_transaction
+
+  def execute_transaction
     if self.status=="pending" && self.sender.valid? && self.receiver.valid? && self.sender.balance >= self.amount
       self.sender.balance -= amount
       self.receiver.balance += amount
@@ -24,6 +27,7 @@ class Transfer
       "Transaction rejected. Please check your account balance."
     end
   end
+  
   def reverse_transfer
     if self.status=="complete"
       self.sender.balance += amount
