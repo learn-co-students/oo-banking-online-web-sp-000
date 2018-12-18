b3be67f2f650298dbdb3d7d85c0670c0875db156
Meisuleen Kased <meisuleen@gmail.com>
2018-12-18 04:02:13 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,7 +28,11 @@ class Transfer
   end
 
   def reverse_transfer
-
+    if @status == "complete"
+      @receiver.balance -= @amount
+      @sender.balance += @amount
+      @status = "reversed"
+    end
   end
 
 
