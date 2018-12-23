e3cd47113b8a0b8394e07582c954c184fe6f8df4
Ariel <arielord@buffalo.edu>
2018-12-23 03:20:35 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,14 @@
 class BankAccount
-
+  attr_accessor :status
+  attr_reader :name, :balance
+  
+  def intialize(name)
+    @name = name
+    @balance = 1000
+    self.status = "open"
+  end
+  
+  def deposit
+    
+  end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,8 @@
 class Transfer
   # your code here
+  attr_accessor
+  
+  def initialize
+    
+  end
 end
