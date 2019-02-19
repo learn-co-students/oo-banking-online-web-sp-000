fb86bc34af9b3d3963af12a8ab7435355102f462
Chovin Carlson <Chovin@users.noreply.github.com>
2019-02-19 23:11:13 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,34 @@
 class BankAccount
-
+  attr_reader :name
+  attr_accessor :balance, :status 
+  
+  def initialize(name)
+    @name = name
+    @status = "open"
+    @balance = 1000
+  end
+  
+  def deposit(amount)
+    if amount >= 0
+      @balance += amount
+    end
+  end
+  
+  def withdraw(amount)
+    if self.valid? && @balance >= amount && amount > 0
+      @balance -= amount
+    end
+  end
+  
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+  
+  def valid?
+    @status == "open" && @balance > 0
+  end
+  
+  def close_account
+    @status = "closed"
+  end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,33 @@
 class Transfer
-  # your code here
+  attr_reader :status, :sender, :receiver, :amount
+  
+  def initialize(sender, receiver, amount)
+    @status = "pending"
+    @amount = amount
+    @sender = sender
+    @receiver = receiver
+  end
+  
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+  
+  def execute_transaction
+    if self.valid? && @status == "pending" && @sender.balance >= @amount
+      @receiver.deposit(@amount)
+      @sender.withdraw(@amount)
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+  
+  def reverse_transfer
+    if self.valid? && @status == "complete" && @receiver.balance >= @amount
+      @sender.deposit(@amount)
+      @receiver.withdraw(@amount)
+      @status = "reversed"
+    end
+  end
 end
