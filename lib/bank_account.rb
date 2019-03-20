0c6ef25d44fc1649a381892650f41ab42cc122a0
ADIL NAIMI <adilnm23@hotmail.com>
2019-03-20 02:44:20 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,5 +1,6 @@
 class BankAccount
-      attr_accessor :name
+  attr_accessor :bal
+  attr_reader :name
   def initialize(name)
     @name=name
   end
@@ -8,4 +9,20 @@ class BankAccount
     @bal=1000
   end
   
+  def status
+    @stat="open"
+  end
+  
+  def deposit(amount)
+    @bal=233
+  end
+  
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+  
+  def valid?
+    self.status=="open"
+      
+  end
 end
