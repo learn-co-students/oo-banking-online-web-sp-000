2dd53aa33a6ef7549a2edb2c76fcfd6289231b0b
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-04-02 18:46:26 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -34,9 +34,9 @@ class Transfer
     end
   end
 
-def reject_transfer
-  self.status = "rejected"
-  "Transaction rejected. Please check your account balance."
-end
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
 
 end
