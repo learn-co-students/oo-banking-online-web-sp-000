4b63fc4590dcf3a582baa6bd00febd454710b24e
Aglae Verduzco <aglae.cv@gmail.com>
2019-04-08 03:41:40 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,38 @@
 class BankAccount
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+
+  end
+
+  def balance
+    @balance
+  end
+
+  def status
+    @status
+  end
+
+  def deposit(deposit)
+    @balance += deposit
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    #status == "open" && balance > 0 ? true : false
+  if @status == "open"
+    true
+  elsif @balance >= 0
+    true
+  else
+    false
+  end
+end
 
 end
