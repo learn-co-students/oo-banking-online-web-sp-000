5a33a5b95cb3e0cb023ab091e86b98d9e10c1406
Aglae Verduzco <aglae.cv@gmail.com>
2019-04-11 01:47:36 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,10 +9,26 @@ class Transfer
   end
 
   def valid?
-    @sender.valid? && @receiver.valid? ? true : false
+    @sender.valid? && @receiver.valid? && @sender.balance > @amount
+
   end
 
   def execute_transaction
-    @sender
+    if @status == "pending" && valid?
+       @sender.balance -= @amount
+       @receiver.balance += @amount
+       @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+     end
   end
+
+  def reverse_transfer
+    if @status == "complete"
+       @receiver.balance -= @amount
+       @sender.balance += @amount
+       @status = "reversed"
+  end
+end
 end
