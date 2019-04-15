36d554b007587fd67bcd61d61e8dc9fd9b52a4e0
Lauren McCoy <lmccoy8911@gmail.com>
2019-04-15 16:19:59 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,12 @@
 class BankAccount
+  attr_accessor :account, :balance, :status 
+  attr_reader :name 
+
+
+  def initialize(name)
+    @name = name 
+    @balance = 1000
+    @status = "open"
+  end
 
 end
