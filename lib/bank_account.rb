7db28b86c2cd83a6f542fc6f015f08b9ec3eb305
Ramon Chavez <rachavez1974@gmail.com>
2019-05-16 01:08:45 -0400

    Finish oo-banking-online-web-sp-000

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,37 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
 
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if @balance > 0 && @status == "open"
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    @status = "closed"
+  end
+
+  def deposit(deposit_amount)
+    @balance += deposit_amount
+  end
 end
+
+
+# b1 = BankAccount.new("Razor")
+# puts b1.display_balance()
+# b1.deposit(500)
+# puts "after deposit #{b1.display_balance()}" 
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,45 @@
+
+require "./lib/bank_account.rb"
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+  
+  def initialize(sender, receiver, amount, status = "pending")
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = status
+    @last_transfer_amount = amount
+  end
+
+  def valid?
+    if @sender.valid? && @receiver.valid?
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+    if !@sender.valid? || @sender.balance < @amount
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    else
+      @receiver.deposit(@amount)
+      @sender.deposit(@amount * -1)
+      @status = "complete"
+      @amount = 0
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      puts @sender.name
+      transfer_back = Transfer.new(@receiver, @sender, @last_transfer_amount)
+      transfer_back.execute_transaction()
+      @status = "reversed"
+    end 
+  end
+
 end
+
