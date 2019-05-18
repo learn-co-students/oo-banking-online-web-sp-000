bb098d09d2bc3321890f1df01d84df1825895a70
Paul Rosero <paul_rosero@yahoo.com>
2019-05-18 22:43:34 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,29 @@
+require 'pry'
 class BankAccount
+  attr_accessor :balance, :status, :amount
+  attr_reader :name
 
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end 
+  
+  def deposit(amount)
+    @balance + amount
+    
+    #binding.pry
+  end
+  
+  def display_balance
+    
+  end
+  
+  def valid?
+    
+  end
+  
+  def close_account
+    
+  end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,22 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount 
+  
+  def initialize(sender, receiver, transfer_amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = 50
+  end
+  
+  def valid?()
+    
+  end
+  
+  def execute_transaction
+    
+  end
+  
+  def reverse_transfer
+    
+  end
 end
