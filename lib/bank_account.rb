97c8ca9e6258ce921e032f3e48b69623fe56e95b
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-06-22 15:49:15 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
 class BankAccount
+  attr_reader :name
+  attr_accessor :status, :balance
 
+  def initialize(name)
+  @name = name 
+  @status = "open"
+  @balance = 1000
+  end
+  
+  def deposit(amount)
+    @balance = (@balance + amount)
+    @balance
+  end
+  
+  def display_balance
+    "Your balance is $#{@balance}." 
+  end
+  
+  def valid?
+    if (@balance > 0 && @status == "open")
+      true 
+    else
+      false 
+    end
+  end
+  
+  def close_account
+    @status = "closed"
+    @status
+  end
 end
