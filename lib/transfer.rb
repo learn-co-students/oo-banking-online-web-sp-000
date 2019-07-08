ac0c06b7e0e99f4609efc3ee712a8d3a217b5ab3
Connor Cowan <ccowan003@comcast.net>
2019-07-08 07:34:53 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,25 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    if @sender.valid? && @receiver.valid?     #checks that both accounts are valid
+      return true                             #valid? method is boolean, checks for errors on an object
+    else                                      #essentially checks if an these objects exist
+      return false
+    end
+  end
+
+  def execute_transaction
+    if !(@sender.valid?)
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
 end
