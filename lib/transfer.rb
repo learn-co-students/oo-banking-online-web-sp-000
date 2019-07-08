b5a84e7998eb3245ba768365574ba3b5bba8dce2
Connor Cowan <ccowan003@comcast.net>
2019-07-08 22:58:28 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,9 +17,22 @@ class Transfer
     end
   end
 
-  def execute_transaction
-    if !(@sender.valid?)
-      return "Transaction rejected. Please check your account balance."
+  def execute_transaction       #executes a successful transaction between two accounts
+    if @sender.balance > @amount && @status == "pending"      #here @sender instance var is receiver of BankAccount balance method (attr_accessor)
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else                                    #rejects a transfer if the @sender account is not valid
+      @status = "rejected"
+        return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"           #condition if transfer was previously executed (complete)
+      @sender.balance += @amount      #reverse @amount(s)
+      @receiver.balance -= @amount
+      @status = "reversed"            #update @status to reversed
     end
   end
 end
