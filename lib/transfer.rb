a6f40f2b0fa860f4e918c203b791b698ae7fce70
Peter Grant <jgrantps@hotmail.com>
2019-07-09 20:34:51 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,59 @@
 class Transfer
-  # your code here
+
+  attr_reader :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    if sender.valid? == true && receiver.valid? == true
+      true
+    end
+  end
+
+  # def execute_transaction
+  #   deposit_amount = @amount
+  #   withdrawl_amount = (@amount*-1)
+  #   if sender.valid? == true
+  #     sender.deposit(withdrawl_amount)
+  #     receiver.deposit(deposit_amount)
+  #     @status = "complete"
+  #     @amount = "Transaction rejected. Please check your account balance."
+  #   else
+  #   amount = "Transaction rejected. Please check your account balance."
+  #   end
+  # end
+
+    def execute_transaction
+      deposit_amount = @amount
+      withdrawl_amount = (@amount *-1)
+      if sender.balance < deposit_amount
+        @status = "rejected"
+        return "Transaction rejected. Please check your account balance."
+      elsif status == "complete"
+        "Transaction is already complete!"
+      else
+        sender.deposit(withdrawl_amount)
+        receiver.deposit(deposit_amount)
+        @status = "complete"
+      end
+    end
+
+    def reverse_transfer
+
+        if @status == "complete"
+        deposit_amount = @amount
+        withdrawl_amount = (@amount *-1)
+        receiver.deposit(withdrawl_amount)
+        sender.deposit(deposit_amount)
+        @status = "reversed"
+      else
+        "sorry, previous transfer was incomplete!"
+      end
+    end
+
 end
