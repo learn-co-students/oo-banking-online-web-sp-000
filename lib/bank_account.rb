661a3efefe26f3b66104a43b1ef91a27f2eb7fd1
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-11 18:14:59 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,34 @@
 class BankAccount
 
+attr_accessor :status, :balance
+attr_reader :name
+
+@@accounts = []
+
+def initialize(name)
+  @name = name
+  @name.freeze
+  @status = "open"
+  @balance = 1000
+  @@accounts << name if !@@accounts.include?(name)
+end
+
+def deposit(amount)
+  @balance += amount
+end
+def display_balance
+  "Your balance is $#{@balance}."
+end
+
+def valid?
+  @status == "open" && @balance > 0
+end
+
+def close_account
+  @status = "closed"
+end
+def self.accounts
+  @@accounts
+end
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,30 @@
+require 'pry'
 class Transfer
-  # your code here
+  attr_accessor :amount, :status, :accounts
+  attr_reader :sender, :receiver
+
+def initialize(sender, receiver, amount)
+  @sender = sender
+  @receiver = receiver
+  @amount = amount
+  @status = "pending"
+end
+
+def valid?
+    @sender.valid? && @receiver.valid?
+end
+
+def execute_transaction
+  while @status == "pending"
+   if !@sender.valid?
+     @status = "rejected"
+     puts "Transaction rejected. Please check your account balance."
+   else
+    @sender.balance -= @amount
+    @receiver.balance += @amount
+    @status = "complete"
+   end
+ end
+end
+binding.pry
 end
