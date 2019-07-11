2541d4753491e1aa304ffb93cddba9d8c6f5a325
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-11 18:25:19 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,16 +15,21 @@ def valid?
 end
 
 def execute_transaction
-  while @status == "pending"
-   if !@sender.valid?
-     @status = "rejected"
-     puts "Transaction rejected. Please check your account balance."
-   else
-    @sender.balance -= @amount
-    @receiver.balance += @amount
-    @status = "complete"
-   end
- end
+  if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+  else
+      @status = "rejected"
+     "Transaction rejected. Please check your account balance."
+  end
 end
-binding.pry
+def reverse_transfer
+  if @status == "complete"
+    @sender.balance += @amount
+    @receiver.balance -= @amount
+    @status = "reversed"
+  end
+end
+# binding.pry
 end
