c5d1078809b8bd54f439d0d1968e4593057e68fb
Lindsey Wells <lindsey.wells84@gmail.com>
2019-07-15 15:11:24 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,34 @@
+require "pry"
+
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(money)
+    @balance = @balance + money
+  end
+  
+  def withdraw(money)
+    @balance = @balance - money
+  end
+  
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+  
+  def valid?
+    self.status == "open" && self.balance > 0
+  end
+  
+  def close_account
+    self.status = "closed"
+  end
+   
 
 end
