20c800192f06f7b2da7957dece9adf6af463e5a1
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-10 23:35:54 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       @status = "complete"
 
     else
-      self.status = "rejected"
+      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
