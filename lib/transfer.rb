381c0bb3b672274808dfb4d2db26febb1ec7a42e
Panela Leung <leungpanela@gmail.com>
2019-09-16 03:55:46 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,12 +1,35 @@
 require "pry"
 
 class Transfer
-  attr_reader  :reciever, :transfer
-  attr_accessor :sender
+  attr_reader  :receiver, :transfer
+  attr_accessor :sender, :status, :amount
 
-  def initialize (transfer, sender, reciever)
-    @sender= sender
-    binding.pry
+  def initialize (sender, receiver, transfer)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = transfer
+  end
+
+  def valid?
+    if sender.valid? && receiver.valid?
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+    if  @status =="pending"
+      if sender.valid? == true
+        sender.deposit(@amount * -1)
+        receiver.deposit(@amount)
+        @status = "complete"
+      else
+        @status = "rejected"
+      end
+    end
+    #   return "Transaction rejected. Please check your account balance."
   end
 
 end
