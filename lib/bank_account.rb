4e40640796bce33272a0e46edc218b3a2db8b179
Panela Leung <leungpanela@gmail.com>
2019-09-16 02:22:13 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
+require "pry"
+
 class BankAccount
+  attr_reader :name
+  attr_accessor :status, :balance
+
+  def initialize (name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(deposit)
+    @balance += deposit
+  end
+
+  def display_balance
+    return "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if @status == "open" && @balance > 0
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    @status = "closed"
+  end
 
 end
