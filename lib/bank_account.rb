9d97b3cecdbe0e7b8f9f3de3feb04df23c053c46
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-09-23 23:24:32 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,34 @@
 class BankAccount
-
+  @@all = []
+  @@allOpen = []
+  @@allClosed = []
+  
+  attr_accessor :balance, :status
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @status = "open"
+    @balance = 1000
+  end
+  
+  def deposit(amount)
+    self.balance = self.balance + amount
+  end
+  
+  def display_balance
+    return "Your balance is $#{self.balance}."
+  end
+  
+  def valid?()
+    self.status == "open" && self.balance > 0
+  end
+  
+  def close_account
+    self.status = "closed"
+  end
+  
 end
+
+#  rspec spec/banking_spec.rb
+#  rspec spec/transfer_spec.rb
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,36 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :receiver, :sender, :status, :amount
+  
+  def initialize(sender_oi, receiver_oi, transfer_amount)
+    @receiver = receiver_oi
+    @sender = sender_oi
+    @status = "pending"
+    @amount = transfer_amount
+  end
+  
+  def valid?
+    self.receiver.valid? && self.sender.valid? && self.sender.balance > self.amount && (self.status == "pending" || self.status == "rejected")
+  end
+  
+  def execute_transaction
+    if self.valid? 
+      self.receiver.balance = self.receiver.balance + self.amount
+      self.sender.balance = self.sender.balance - self.amount
+      self.status = "complete"
+    else 
+      self.status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+    
+  def reverse_transfer
+    if self.status == "complete"
+      self.receiver.balance = self.receiver.balance - self.amount
+      self.sender.balance = self.sender.balance + self.amount
+      self.status = "reversed"
+    end
+  end
 end
+
+#  rspec spec/transfer_spec.rb
