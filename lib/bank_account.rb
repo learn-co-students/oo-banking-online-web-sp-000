b484b7daafe7fc5a3c3a78eef6a6da555e3cbc9d
Andrew Williams <1williamsaj@gmail.com>
2019-10-08 03:27:45 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
 class BankAccount
 
+  attr_accessor :balance, :status # We create getter and setter methods for these attributes since they will be modified.
+  attr_reader :name # Since we do NOT want our name to change once initialized, we just set a reader method.
+
+  def initialize(name) # We initialize our BankAccount with a name, setting our attributes accordingly.
+    @name = name
+    @balance = 1000
+    @status = 'open'
+  end
+
+  def deposit(money) # Takes the argument of 'money', converts it to an integer, and adds it to our 'balance'.
+    self.balance += money.to_i
+  end
+
+  def display_balance # Simply outputs a string with our balance attribute inserted inside.
+    "Your balance is $#{@balance}."
+  end
+
+  def valid? # Checks to see if status and balance meet necessary conditions. If not, it returns false.
+    self.status == 'open' && self.balance > 0 ? true : false
+  end
+
+  def close_account # Simply updates the status of the Class instance to 'closed'.
+    self.status = 'closed'
+  end
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,40 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = 'pending'
+    @amount = amount
+  end
+
+  def valid? # Since our 'sender' and 'receiver' attributes are Class instances of 'BankAccount' class, we can use the 'valid?' method on them.
+    @sender.valid? && @receiver.valid? ? true : false # We compare to see if both are true.
+  end
+
+  def execute_transaction # We first use the method to check and see if our transfer is valid and has a status of 'pending'
+    if valid? && @status == 'pending'
+      if @sender.balance > amount # We then check to see if the amount the 'sender' is transferring is less than the amount in their balance.
+        @sender.balance -= amount # If so, we execute a transfer, removing funds from the sender and giving them to the receiver.
+        @receiver.balance += amount # We finish it off by changing our status to 'complete', as to avoid processing the transfer again.
+        @status = 'complete'
+      else # If the sender is transferring more than their account allows, we reject the transfer.
+        @status = 'rejected'
+        "Transaction rejected. Please check your account balance."
+      end
+    else # If the transfer does not meet one or both of the requirements above in the '&&', we reject the transfer immediately.
+      @status = 'rejected'
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == 'complete' # First, we check to see if the transfer status is 'complete'. If not, we know that no transfer took place.
+      @sender.balance += amount # After confirming a transfer, we simply reverse the math, returning the sender's funds to their balance.
+      @receiver.balance -= amount
+      @status = 'reversed' # We update the status with 'reverse'.
+    end
+  end
+
 end
