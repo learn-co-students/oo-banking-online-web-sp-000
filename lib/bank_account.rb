bd3bc6e158c4618f6bb8a9b918afd474254ff126
Blythe Pollard <blytheepollard@gmail.com>
2019-10-11 18:00:23 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,23 @@
+require 'pry'
 class BankAccount
+ #one instance of class can transfer money to another instance thru Transfer class
+ attr_accessor :balance, :status
+ attr_reader :name
 
+ def initialize(name)
+   @name = name
+   @balance = 1000
+   @status = "open"
+ end
+ 
+ def deposit(new_amount)
+   @balance = self.balance + new_amount
+   @balance
+ end
+   
+  def display_balance
+     "Your balance is $#{self.balance}."
+  end
+  
+  def 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,7 @@
 class Transfer
-  # your code here
+  # space for a transaction between two instances of bank acct class
+  #checks validity of account before transaction occurs
+  #can reject a transfer if accts arent valid or if no money
+  #start in "pending", end as "complete" or "rejected"
+  #complete transfer can be reversed, go into "reversed" status
 end
