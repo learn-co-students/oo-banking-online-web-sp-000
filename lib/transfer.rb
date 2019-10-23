2f5a4601de24bd268aeea4182144e2ca9953d277
Anthony Tilelli <anthony@tilelli.me>
2019-10-23 17:24:08 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,15 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount
+  def initilized(sender, receiver,amount)
+  end
+  
+  def valid?
+  end
+  
+  def execute_transaction
+  end
+  
+  def reverse_tranfer
+  end
+
 end
