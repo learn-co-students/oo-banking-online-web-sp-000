9fc417f941eed0bd1c64281e68f51a255d52544d
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-26 16:42:52 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,4 +26,12 @@ class Transfer
        return "Transaction rejected. Please check your account balance."
     end
   end
+
+  def reverse_transfer
+    if self.execute_transaction == "complete"
+      self.sender.balance = self.sender.balance + (amount)
+      self.receiver.balance = self.receiver.balance - (amount)
+      @status = "reversed"
+    end
+  end
 end
