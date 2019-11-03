f9bbf3858eb9bdf9de1c3af468f98e633616ef48
Haim D <haim11293@aol.com>
2019-11-03 20:29:14 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,12 +14,12 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
+    if valid? && @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
     else
-      # @status = "rejected"
+      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
