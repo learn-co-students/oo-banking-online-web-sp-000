6e40041a347d6fcfedbe0d218d991e79e46a6753
Jason J Cruz <jaycru06@yahoo.com>
2019-12-16 05:09:31 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,73 @@
-class BankAccount
+# BankAccount class: 
 
+# #initialize: 
+# Can initialize a bank account 
+# initialize with a name 
+# always initializes with a balance of 1000 
+# always initializes with a status of 'open' 
+# can't change its name 
+
+# #deposit: 
+# can deposit money into its account 
+
+# #display_balance: 
+# can display its balance 
+
+# #valid: 
+# is valid with an open status and a balance greater than 0 
+
+# #close_account:
+# can close its account 
+
+class BankAccount
+  attr_accessor :balance, :status  
+  attr_reader :name 
+  
+  def initialize(name)
+    @name = name 
+    @balance = 1000 
+    @status = "open" 
+  end 
+  
+  def deposit(money) 
+    @balance += money  
+  end 
+  
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end 
+  
+  def valid?
+    self.balance > 0 && self.status.include?("open") ? true : false 
+  end 
+  
+  def close_account 
+    self.status = "closed" 
+  end 
+  
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,52 @@
+# Transfer
+  # initialize
+  #   can initialize a Transfer (FAILED - 1)
+  #   initializes with a sender (FAILED - 2)
+  #   initializes with a receiver (FAILED - 3)
+  #   always initializes with a status of 'pending' (FAILED - 4)
+  #   initializes with a transfer amount (FAILED - 5)
+  # #valid?
+  #   can check that both accounts are valid (FAILED - 6)
+  #   calls on the sender and receiver's #valid? methods (FAILED - 7)
+  # #execute_transaction
+  #   can execute a successful transaction between two accounts (FAILED - 8)
+  #   each transfer can only happen once (FAILED - 9)
+  #   rejects a transfer if the sender does not have enough funds (does not have a valid account) (FAILED - 10)
+  # #reverse_transfer
+  #   can reverse a transfer between two accounts (FAILED - 11)
+  #   it can only reverse executed transfers (FAILED - 12)
+
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :transfer_amount
+  attr_reader :status 
+  
+  def initialize(sender, receiver, transfer_amount)
+    @sender = sender 
+    @receiver = receiver 
+    @transfer_amount = transfer_amount
+    @status = "pending"
+  end 
+  
+  def amount 
+    @transfer_amount = 50 
+  end 
+  
+  def valid?
+    binding.pry 
+    BankAccount.valid?
+  end 
+  
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
