3d0a19862ba17e23af006e7cb01494cd0fd74e28
Jason J Cruz <jaycru06@yahoo.com>
2019-12-16 20:39:08 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,24 +1,3 @@
-# BankAccount class: 
-
-# #initialize: 
-# Can initialize a bank account 
-# initialize with a name 
-# always initializes with a balance of 1000 
-# always initializes with a status of 'open' 
-# can't change its name 
-
-# #deposit: 
-# can deposit money into its account 
-
-# #display_balance: 
-# can display its balance 
-
-# #valid: 
-# is valid with an open status and a balance greater than 0 
-
-# #close_account:
-# can close its account 
-
 class BankAccount
   attr_accessor :balance, :status  
   attr_reader :name 
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,21 +1,3 @@
-# Transfer
-  # initialize
-  #   can initialize a Transfer (FAILED - 1)
-  #   initializes with a sender (FAILED - 2)
-  #   initializes with a receiver (FAILED - 3)
-  #   always initializes with a status of 'pending' (FAILED - 4)
-  #   initializes with a transfer amount (FAILED - 5)
-  # #valid?
-  #   can check that both accounts are valid (FAILED - 6)
-  #   calls on the sender and receiver's #valid? methods (FAILED - 7)
-  # #execute_transaction
-  #   can execute a successful transaction between two accounts (FAILED - 8)
-  #   each transfer can only happen once (FAILED - 9)
-  #   rejects a transfer if the sender does not have enough funds (does not have a valid account) (FAILED - 10)
-  # #reverse_transfer
-  #   can reverse a transfer between two accounts (FAILED - 11)
-  #   it can only reverse executed transfers (FAILED - 12)
-
 class Transfer
   attr_accessor :sender, :receiver, :amount, :status 
   
@@ -28,12 +10,10 @@ class Transfer
   
   
   def valid?
-    #sender.balance >= amount && receiver.balance >= amount ? true : false 
     sender.valid? && receiver.valid? 
   end 
   
   def execute_transaction
-    #binding.pry 
     if valid? && self.status == "pending" && sender.balance >= amount  
       sender.balance -= amount 
       receiver.balance += amount 
