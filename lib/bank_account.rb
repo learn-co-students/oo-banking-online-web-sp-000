c5002958ab8b52ac1d3547475e18f7d0ae346e56
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-11 22:48:56 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
 class BankAccount
 
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    @deposit = []
+  end
+
+  # can deposit money into its account
+  def deposit(money)
+    @balance += money
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  # is valid with an open status and a balance greater than 0
+  def valid?
+    @status == "open" && @balance > 0 ? true : false
+  end
+
+  def close_account
+    @status = "closed"
+  end
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,32 @@
 class Transfer
-  # your code here
+
+  attr_accessor :status, :sender, :receiver, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  # can check that both accounts are valid
+  # calls on the sender and receiver's #valid? methods
+  def valid?
+    @sender.valid? && @receiver.valid? ? true : false
+  end
+
+  # can execute a successful transaction between two accounts
+  # each transfer can only happen once
+  # rejects a transfer if the sender does not have enough funds (does not have a valid account)
+  def execute_transaction
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
 end
