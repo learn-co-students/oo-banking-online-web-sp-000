c9084f37e5ab29721c64178cee9110ed80d8e70c
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-12 19:53:39 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,14 +18,41 @@ class Transfer
   # can execute a successful transaction between two accounts
   # each transfer can only happen once
   # rejects a transfer if the sender does not have enough funds (does not have a valid account)
+  # def execute_transaction
+  #   if @sender.balance < @amount && @sender.close_account == "closed"
+  #     @status = "rejected"
+  #     "Transaction rejected. Please check your account balance."
+  #   elsif @status == "complete"
+  #     puts "Transaction has already been executed"
+  #   else
+  #     @sender.deposit(-@amount)
+  #     @receiver.deposit(@amount)
+  #     @status = "complete"
+  #   end
+  # end
+
+
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
-    else
+    if @status == "complete"
+      puts "Transaction has already been executed"
+    elsif @sender.balance < @amount || !(@receiver.valid?)
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
+    else @sender.balance > @amount && @receiver.valid?
+      @sender.deposit(-@amount)
+      @receiver.deposit(@amount)
+      @status = "complete"
+    end
+  end
+
+
+  # can reverse a transfer between two accounts
+  # it can only reverse executed transfers
+  def reverse_transfer
+    if @status == "complete"
+      @sender.deposit(@amount)
+      @receiver.deposit(-@amount)
+      @status = "reversed"
     end
   end
 
