203dbfa6783a791284e6969e2a688ef59d2ce8e6
sheriefCloud <sherief@farid.cloud>
2020-01-12 19:12:36 -0800

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 class Transfer
-  # your code here
+  
+  attr_reader :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    @receiver.valid? && @sender.valid?
+  end
+
+  def execute_transaction
+    if !@status == "pending" #|| !@sender.balance > amount
+      @status = "rejected"
+      p "Transaction rejected. Please check your account balance."
+    else
+      binding.pry
+      @sender.balance -= amount
+      @receiver.balance += amount
+      @status = "complete"  
+    end
+
+  end
 end
+
+
+# if @receiver.valid? && @sender.valid?
+#   @status = "pending"
+#   return true
+# else
+#   @status="rejected"
+#   p "Transaction rejected. Please check your account balance."
+# end
+
