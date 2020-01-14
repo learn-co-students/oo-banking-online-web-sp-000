463ab00f228d15a19c4bca5525d6b5d7a7cdfd59
Will Carter <will.carter@fergusllc.com>
2020-01-14 15:14:58 -0700

    Done.

--- spec/transfer_spec.rb
+++ spec/transfer_spec.rb
@@ -1,4 +1,3 @@
-require 'pry'
 describe 'Transfer' do
 
   let(:avi) { BankAccount.new("Avi") }
@@ -51,7 +50,7 @@ describe 'Transfer' do
     let(:amanda) { BankAccount.new("Amanda") }
     let(:transfer) { Transfer.new(amanda, avi, 50) }
     let(:bad_transfer) { Transfer.new(amanda, avi, 4000) }
-
+    
     it "can execute a successful transaction between two accounts" do
       transfer.execute_transaction
       expect(amanda.balance).to eq(950)
