edc6bad71fed53b7ce6c0f1d81c83e9d924986dc
cantstopcoding <jonathan_koss@yahoo.com>
2020-01-23 22:10:22 -0500

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end 
+
+  def valid?
+    if @sender.status == BankAccount.new(@sender).status && @receiver.status == BankAccount.new(@receiver).status
+      true 
+    else
+      false
+    end 
+  end 
 end
