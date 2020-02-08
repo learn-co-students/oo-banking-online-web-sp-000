1ad0446424b97a5e6987d0247517cd705d52effa
Mark A Guza <guzamaguza@gmail.com>
2020-02-08 23:48:15 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
 class BankAccount
+  attr_accessor :status, :balance
+  attr_reader :name #should only be allowed to read not write
+
+  def initialize(name)
+    @name = name
+    @status = "open"
+    @balance = 1000
+  end
+
+  def deposit(amount)
+    @balance += amount
+    return @balance
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    @balance > 0 && @status == "open" ?  true : false
+  end
+
+  def close_account
+    @status = "closed"
+  end
 
 end
