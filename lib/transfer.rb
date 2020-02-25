69c5e01b66c11f2b517fbb8fde054d903212d755
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-25 08:48:32 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -8,11 +8,11 @@ class Transfer
   end
 
   def valid?
-    sender.valid? && receiver.valid? && status == "pending"
+    sender.valid? && receiver.valid?
   end
 
   def execute_transaction
-    if sender.balance > amount && self.valid?
+    if sender.balance > amount && valid? && status == "pending"
       sender.deposit(-amount)
       receiver.deposit(amount)
       @status = "complete"
@@ -23,10 +23,13 @@ class Transfer
   end
 
   def reverse_transfer
-    if status == "complete"
+    if sender.balance> amount && valid? && status == "complete"
       sender.deposit(amount)
       receiver.deposit(-amount)
       @status = "reversed"
+    else
+      @status = "rejected"
+      "Transaction rejecte. Please check your account balance."
     end
   end
 
