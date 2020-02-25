9675009e7c92bd5a36a2fe42390173fbd1fb5201
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-25 08:41:33 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,35 @@
 class Transfer
   # your code here
+  attr_reader :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender, @receiver, @amount = sender, receiver, amount
+    @status = "pending"
+  end
+
+  def valid?
+    sender.valid? && receiver.valid? && status == "pending"
+  end
+
+  def execute_transaction
+    if sender.balance > amount && self.valid?
+      sender.deposit(-amount)
+      receiver.deposit(amount)
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if status == "complete"
+      sender.deposit(amount)
+      receiver.deposit(-amount)
+      @status = "reversed"
+    end
+  end
+
+  # Using @status instead of giving it an attr_writer so that the status
+  # can only be changed internally
 end
