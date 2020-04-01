f3da65fd43ccfd02bbc4041d92e5eeaccdf3fd44
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-04-01 18:58:39 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance < @amount || !valid?
+    if !valid? || @sender.balance < @amount
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
     elsif @status != "complete"
