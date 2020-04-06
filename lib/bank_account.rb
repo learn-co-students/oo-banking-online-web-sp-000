e11364bc0351651583df07de74c835d5132c07dc
C. Leow <chienleow1@gmail.com>
2020-04-06 15:34:46 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -18,7 +18,7 @@ class BankAccount
   end
 
   def valid?
-    if @status = "open" && @balance > 0
+    if @status == "open" && @balance > 0
       return true
     else
       return false
@@ -26,7 +26,7 @@ class BankAccount
   end
   
   def close_account
-  
+    @status = "closed"
   end
 
 end
