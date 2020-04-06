f1c685b6454da1a17725daeb32359ba444b72640
C. Leow <chienleow1@gmail.com>
2020-04-06 15:39:47 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -18,11 +18,7 @@ class BankAccount
   end
 
   def valid?
-    if @status == "open" && @balance > 0
-      return true
-    else
-      return false
-    end
+    @status == "open" && @balance > 0
   end
   
   def close_account
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -8,4 +8,9 @@ class Transfer
     @status = "pending"
     @amount = amount
   end
+  
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+  
 end
