ff4aeb57d25a169a0f84e141c3182373ea441051
C. Leow <chienleow1@gmail.com>
2020-04-06 16:12:29 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,4 +13,23 @@ class Transfer
     sender.valid? && receiver.valid?
   end
   
+  def execute_transaction
+    if valid? && sender.balance > @amount && @status == "pending"
+      sender.balance -= @amount
+      receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      receiver.balance -= @amount
+      sender.balance += @amount
+      @status = "reversed"
+    end
+  end
+  
 end
