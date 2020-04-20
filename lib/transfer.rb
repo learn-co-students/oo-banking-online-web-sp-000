94216bb924da4f32ed82a45c706f913196aa862b
Elliot Blanchard <public@elliotblanchard.com>
2020-04-20 00:54:17 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,42 @@
+require 'pry'
+
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    if sender.valid? && receiver.valid?
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+    if @status == "pending"
+      if sender.balance > @amount && sender.valid? && receiver.valid?
+        sender.deposit(@amount * -1)
+        receiver.deposit(@amount)
+        @status = "complete"
+      else
+        @status = "rejected"
+        return "Transaction rejected. Please check your account balance."
+      end
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      sender.deposit(@amount)
+      receiver.deposit(@amount * -1)
+      @status = "reversed"
+    end
+  end
+
 end
