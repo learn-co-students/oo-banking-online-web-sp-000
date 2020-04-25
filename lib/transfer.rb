7acc9905a3a0d92802f95fe880900c697cf2430b
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-25 02:20:50 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,30 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+  
+  def valid?
+    BankAccount.select {|account| account.name == @receiver} && BankAccount.select {|account| account.name == @sender}
+  end
+  
+  def execute_transaction
+    if valid? && @sender >= @amount
+      @sender -= @amount
+      @receiver += @amount
+    else
+      "rejected"
+    end
+  end
+  
+  def reverse_transfer
+    if execute_transaction 
+      @sender += @amount
+      @receiver -= @amount
+    end
+  end
+  
 end
