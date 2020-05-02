0d774fd55d082c0def03a2ed17439920a7831f0b
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-05-02 22:08:19 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,34 @@
+require 'pry'
+
 class Transfer
-  # your code here
+  
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+  
+  
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender 
+    @receiver = receiver 
+    @amount = amount
+    @status = "pending"
+  end
+  
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+  
+  def execute_transaction
+    if self.valid? && @status == "pending"
+      @sender.withdraw(@amount)
+      @receiver.deposit(@amount)
+      @status = "complete"
+    else 
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+    
+  end
+  
+  
 end
