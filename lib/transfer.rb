cecd6beda6d2eb1a90472c4b2396787790fa221c
Sonja Parsell <hello@sparsell.com>
2020-05-12 16:36:00 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,24 +1,37 @@
-class Transfer
-  attr_accessor :sender, :receiver, :amount, :status
+require 'pry'
+  class Transfer
+    attr_accessor :sender, :receiver, :amount, :status
 
-  def initialize (sender, receiver, amount)
-    @sender = sender
-    @receiver = receiver
-    @amount = amount
-    @status = "pending"
+    def initialize (sender, receiver, amount)
+      @sender = sender
+      @receiver = receiver
+      @amount = amount
+      @status = "pending"
+
+    end
+
+    def valid?
+      sender.valid? && receiver.valid?
+    end
+
+    def execute_transaction
+      if valid? && sender.balance >= @amount && self.status == "pending"#rejects a transfer if the sender does not have enough funds
+        sender.balance -= @amount
+        receiver.balance += @amount#can execute a successful transaction between two accounts
+        self.status = "complete"
+      else
+        self.status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      end
+    end
+
+
+    def reverse_transfer
+      if valid? && receiver.balance >= @amount && self.status == "complete"#rejects a transfer if the sender does not have enough funds
+        sender.balance += @amount
+        receiver.balance -= @amount#can execute a successful transaction between two accounts
+        self.status = "reversed"
+      end
+    end
 
   end
-
-  def valid?
-    sender.valid? && receiver.valid?
-  end
-
-  def execute_transaction
-
-  end
-
-  def reverse_transfer
-
-  end
-
-end
