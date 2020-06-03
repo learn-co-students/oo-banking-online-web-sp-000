749bafe01da6e77304a14d0f8568e5dfff8dfa4f
Jung Lee <lee.jung.s95@gmail.com>
2020-06-03 03:27:11 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
-class BankAccount
+require 'pry'
 
+class BankAccount
+  attr_reader :name
+  attr_accessor  :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = 'open'
+  end
+
+  def deposit(amount)
+    @balance = @balance + amount
+  end
+
+  def display_balance
+    return "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if self.balance > 0 && self.status == 'open'
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    @status = 'closed'
+  end
 end
