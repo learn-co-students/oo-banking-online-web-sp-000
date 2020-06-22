ded469b90e36d3a10e9b2ffb7c965456230e0c4c
YUKIKO IKEDA <applelele@gmail.com>
2020-06-22 23:28:01 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,40 @@
+require "pry"
+
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, status = "pending", amount)
+    @sender = sender
+    @receiver = receiver
+    @status = status
+    @amount = amount
+  end
+
+  def valid?
+    if @sender.valid? && @receiver.valid?
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+    if valid? && @sender.balance > amount && @status == "pending"
+      @sender.balance -= amount
+      @receiver.balance += amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += amount
+      @receiver.balance -= amount
+      @status = "reversed"
+    end
+  end
+
 end
