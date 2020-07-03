fa9c54f435f7cbfae94e3b139855268a132a37b0
Dean Erickson <dmepilot@yahoo.com>
2020-07-03 12:11:15 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,7 +3,7 @@ class BankAccount
   attr_reader :name
   attr_accessor :balance, :status
 
-  def initialize(name)
+  def initialize(name) 
     @name=name
     @balance = 1000
     @status = 'open'
