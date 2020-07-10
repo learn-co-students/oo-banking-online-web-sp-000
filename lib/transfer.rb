f1fef2def41f6390c9217d2bff919ba1779d2ae8
Serhiy Bardysh <sbardysh@tutanota.com>
2020-07-10 18:49:53 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,37 @@
 class Transfer
-  # your code here
+
+  attr_accessor :status, :sender, :receiver, :amount
+
+  def initialize(sender, receiver, amount)
+    @status = "pending"
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid? ? true : false
+  end
+
+  def execute_transaction
+
+    if
+       @sender.balance > @amount && @status == "pending" && valid?
+       @sender.balance -= @amount
+       @receiver.balance += @amount
+       @status = "complete"
+
+    else @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+       @sender.balance += @amount
+       @receiver.balance -= @amount
+       @status = "reversed"
+    end
+  end
+
 end
