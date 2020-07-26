0db2eebd639a9eebdd81eeffb34da57d80035767
nisticism <nistacular@gmail.com>
2020-07-25 17:17:49 -0700

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
     end
 
     def execute_transaction
-        if sender.valid? && receiver.valid? && @status == "pending" && sender.balance > @amount
+        if valid? && @status == "pending"
           sender.balance -= amount
           receiver.balance += amount
         else
