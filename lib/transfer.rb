79b4aa8ceddec8b2dea1f82574ce3568d9cadbee
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-08-05 01:21:57 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,38 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender=sender
+    @receiver=receiver
+    @status = "pending"
+    @amount=50
+  end
+
+  def valid?
+    if @sender.valid? && @receiver.valid?
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+    if valid? && @sender.balance > amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return"Transaction rejected. Please check your account balance."
+    end
+  end
+
+  
+
+
+
+
+
+
+
 end
