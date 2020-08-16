91f4cce02846302a2a9fb67f2a7a3a68f04cd8a5
carriemathieu <carrieburks@gmail.com>
2020-08-16 05:28:47 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,52 @@
 class Transfer
-  # your code here
-end
+  # The Transfer class acts as a space for a transaction between two instances of the bank account class.
+  # check the validity of the accounts before the transaction occurs
+
+  attr_accessor :sender, :receiver, :amount, :last_transaction
+
+  def initialize(sender, receiver, amount)
+    # initializes with a sender 
+    @sender = sender
+    # initializes with a receiver 
+    @receiver = receiver
+    # always initializes with a status of 'pending' 
+    @status = "pending"
+    # initializes with a transfer amount
+    @amount = amount
+  end
+
+  def execute_transaction
+    # they can be executed and go to a "complete" state.
+    # can execute a successful transaction between two accounts 
+    # each transfer can only happen once 
+    if valid? && @status != "complete" && @sender.balance > @amount && @sender.status != "closed"
+      @sender.deposit(@amount * - 1)
+      @receiver.deposit(@amount)
+      @status = "complete"
+    elsif @status == "complete"
+      return "Transaction was already completed"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def status
+    @status
+  end
+
+  def valid?
+    # Checks that both accounts are valid
+    sender.valid? && receiver.valid?
+  end
+
+  def reverse_transfer
+     # Reverse a transfer between two accounts 
+     # Can only reverse executed transfers 
+     if @status == "complete"
+        @sender.deposit(@amount)
+        @receiver.deposit(@amount * - 1)
+        @status = "reversed"
+     end
+  end
+ end
