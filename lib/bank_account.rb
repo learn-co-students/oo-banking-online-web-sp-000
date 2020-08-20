bcb69abda19c1b8dc3fdd8004d97859e53de4a3e
Nick Lanese <nlanese21@gmail.com>
2020-08-20 23:43:48 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,53 @@
 class BankAccount
 
+  def initialize(inputName)
+    @name = inputName
+    @balance = 1000
+    @status = "open"
+  end
+
+  def balance=(number)
+    @balance = number
+  end
+
+  def name
+    @name
+  end
+
+  def balance
+    @balance
+  end
+
+  def status
+    @status
+  end
+
+  def deposit(input)
+    @balance = @balance + input
+  end
+
+  def display_balance
+    return "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if (@balance > 0)
+      if (@status == "open")
+        return true
+      else
+        return false
+      end
+    else
+      return false
+    end
+  end
+
+  def close_account
+    @status = "closed"
+  end
+
+  def status=(input)
+    @status = input
+  end
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,64 @@
 class Transfer
-  # your code here
+
+  def initialize(sender, receiver, amt)
+    @sender = sender
+    @receiver = receiver
+    @amount = amt
+    @status = "pending"
+    @prevSendBal = sender.balance
+    @prevRecBal = receiver.balance
+  end
+
+  def valid?
+    if (@sender.valid? && @receiver.valid?)
+      return true
+    else
+      return false
+    end
+  end
+
+  def sender
+    @sender
+  end
+
+  def sender=(input)
+    @sender = input
+  end
+
+  def receiver
+    @receiver
+  end
+
+  def status
+    @status
+  end
+
+  def amount
+    @amount
+  end
+
+  def execute_transaction
+    if (@status != "complete")
+      if (self.valid? && (@sender.balance - @amount >= 0))
+        @sender.balance=(@sender.balance - @amount)
+        @receiver.balance=(@receiver.balance + @amount)
+        @status = "complete"
+      else
+        @status = "rejected"
+        return "Transaction rejected. Please check your account balance."
+      end
+    else
+      return
+    end
+  end
+
+  def reverse_transfer
+    if (@status == "complete")
+      @sender.balance = @prevSendBal
+      @receiver.balance = @prevRecBal
+      @status = "reversed"
+    end
+  end
+
+
 end
