a485eb70785d129c38b2ef1f55273fe48d6bf1b2
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-20 20:12:53 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,34 @@
 class BankAccount
+attr_reader :name
+attr_accessor :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    (@status != "closed" && @balance != 0)  ? true : false
+  end
+
+
+  def close_account
+    @status = "closed"
+  end
 
 end
+
+
+#transfer money between bank accounts through creating an instance of Transfer
+#kinda like creating a transaction
+#Transfer validates payment before sending, status starts as pending, then either completed or rejected. also a payment u cancel is "reversed" status
+#do u see how modeling objects make it so much easier to understand/code?
