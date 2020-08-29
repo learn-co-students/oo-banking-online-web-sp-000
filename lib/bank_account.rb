379a4845552ffbb1099f220033bee2da51c66670
John Stassen <john.stassen@gmail.com>
2020-08-29 01:34:38 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,35 @@
 class BankAccount
 
+  attr_reader :name
+  attr_accessor :balance, :status, :old_balance
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount)
+    @old_balance = @balance
+    @balance = @balance + amount
+  end
+
+  def display_balance
+    return "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if @status == "open" && @balance > 0
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    @status = "closed"
+    @balance = 0
+  end
+
+
 end
