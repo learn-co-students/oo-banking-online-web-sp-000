ca771defc73419264f16e12de82f0c21fc382aa6
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-09-01 00:51:29 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,37 @@
 class BankAccount
+  
+  attr_accessor :name, :balance, :status
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def name=(name)
+    raise NoMethodError if self.name
+    @name = name
+  end
+  
+  
+  def deposit(amount)
+    self.balance += amount
+  end
+  
+  def withdraw(amount)
+    self.balance -= amount
+  end
+  
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+  
+  def valid?
+    self.status == "open" && self.balance > 0
+  end
+  
+  def close_account
+    self.status = "closed"
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,47 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  @@all = []
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+    self.class.all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def valid?
+    self.sender.valid? && self.receiver.valid?
+  end
+  
+  def completed?
+    all_completed = self.class.all.select {|transfer| transfer.status == "complete"}
+    all_completed.include?(self)
+  end
+  
+  def execute_transaction
+    if !self.valid? || self.sender.balance < self.amount
+      self.status = 'rejected'
+      "Transaction rejected. Please check your account balance."
+    elsif !self.completed?
+      self.sender.withdraw(amount)
+      self.receiver.deposit(amount)
+      self.status = "complete"
+    end
+  end
+  
+  def reverse_transfer
+    if self.completed?
+      self.receiver.withdraw(amount)
+      self.sender.deposit(amount)
+      self.status = "reversed"
+    end
+  end
+  
 end
