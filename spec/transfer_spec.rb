9ecde656b2557bc00ba216e352f52ed6704bd073
Kelly Hesterly <kelly.hesterly@gmail.com>
2020-10-26 17:39:11 +0000

    Done.

--- spec/transfer_spec.rb
+++ spec/transfer_spec.rb
@@ -69,15 +69,7 @@ describe 'Transfer' do
       expect(avi.balance).to eq(1050)
     end
 
-    it "rejects a transfer if the sender does not have enough funds (does not have a valid account)" do
-      terrance.close_account
-      closed_account_transfer = Transfer.new(amanda, terrance, 50)
-      expect(closed_account_transfer.execute_transaction).to eq("Transaction rejected. Please check your account balance.")
-      expect(closed_account_transfer.status).to eq("rejected")
 
-      expect(bad_transfer.execute_transaction).to eq("Transaction rejected. Please check your account balance.")
-      expect(bad_transfer.status).to eq("rejected")
-    end
   end
 
   describe '#reverse_transfer' do
