4e55c08bab2885f39acad8fdbe385ecfd004aac5
Will Studabaker <will.studabaker@gmail.com>
2020-11-03 16:46:38 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,21 @@
 class BankAccount
-
+  attr_accessor :balance, :status
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  def deposit(amount)
+    @balance += amount
+  end
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+  def valid?(transfer_amount = 0)
+    self.status == "open" && self.balance > transfer_amount ? true : false
+  end
+  def close_account
+    self.status = "closed"
+  end
 end
