939800c1dd327d2cb9ffe7033a537b08f7caaf3c
Carrie Jones <c.jessica.jones@gmail.com>
2021-02-02 16:17:06 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,36 @@
 class BankAccount
 
+  attr_accessor :status, :balance, :previous_balance
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @previous_balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(amt)
+    @previous_balance = @balance
+    total = @balance
+    total = (total + amt)
+    @balance = total
+  end
+  
+  def display_balance
+    return "Your balance is $#{@balance}."
+  end
+  
+  def valid?
+    if @status == "open" && @balance > 0
+      true
+    else
+      false
+    end
+  end
+  
+  def close_account
+    @status = "closed"
+  end
+  
 end
