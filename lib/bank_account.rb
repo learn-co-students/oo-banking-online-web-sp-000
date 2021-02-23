5cd1f4401464eba02f206231c1d158f355adc146
HELEN HARMAN <hlharman@bellsouth.net>
2021-02-23 02:05:40 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,27 +2,32 @@ require 'pry'
 
 class BankAccount
   attr_accessor :name, :status, :balance
-
-  def initialize(name)
+  
+  
+    def initialize(name="avi")
     @status = "open"
     @balance = 1000 
    @name = name.freeze
      #begin
-      if self.name != name.freeze 
-     raise FrozenError.new "Frozen name cannot be changed" 
+    #   if self.name != "avi" 
+    # raise FrozenError.new "Frozen name cannot be changed"  
    # rescue 
-    #avi.name = avi 
+    #self.name = avi 
         # FrozenError
-      end 
     end 
   
   
-  def deposit(deposit)
-    balance + deposit
- end 
+  def deposit(balance)
+    deposit = 1000
+    binding.pry 
+  #     [1] pry(#<BankAccount>)> balance => 1000
+  #     [2] pry(#<BankAccount>)> balance + deposit => 2000
+  #     [3] pry(#<BankAccount>)> deposit => 1000
+  new_balance = balance + deposit  
+  end 
 
 
 def display_balance 
-  puts "Your balance is $#{avi.balance}." 
+  puts "Your balance is $#{new_balance}."  
 end 
 end 
