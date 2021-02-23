c079cdb6ff24b045b022b0a9fe969e54e0f1c933
HELEN HARMAN <hlharman@bellsouth.net>
2021-02-23 20:27:03 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,7 +2,6 @@ require 'pry'
 
 class BankAccount
   attr_accessor  :status, :balance
-  @name 
   
 
   def initialize(name="Avi")
@@ -15,32 +14,23 @@ class BankAccount
     @name 
    end 
    
-  def deposit(deposit)
-   current_balance = (balance + deposit) 
+  def deposit(entry)
+    @balance = (@balance + entry)
   end 
-  #but Rspec wants balance to be $2000
-  #rspec says wrong number of arguments, but when I run it with no arguments, I get a perpetual loop
-
 
 def display_balance
-  current_balance = balance + deposit
-  puts "Your balance is #{$current_balance}." 
-end 
-# [1] pry(#<BankAccount>)> deposit
-# => 1000
-# [2] pry(#<BankAccount>)> current_balance = balance + deposit
-# => 2000
-# [3] pry(#<BankAccount>)> current_balance
-# => 2000
+  return "Your balance is $#{@balance}."
+end
+
 
 def valid? 
-  if status = "open" && balance > 0
+  if status = "open" && @balance > 0
   end 
 end 
   
 
 # describe '#valid?' do 
-#     it "is valid with an open status and a balance greater than 0" do
+#     it "is valid with an open status and a balance greater than 0" do 
 #       @broke = BankAccount.new("Kat Dennings")
 #       @broke.balance = 0
 #       @closed = BankAccount.new("Beth Behrs")
