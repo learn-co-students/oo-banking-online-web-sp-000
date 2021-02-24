dab0760b24f44100ec98952391f6c2cb093a2462
HELEN HARMAN <hlharman@bellsouth.net>
2021-02-24 23:56:11 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,8 +1,37 @@
 class Transfer
   attr_accessor :sender, :receiver 
 
-   def initialize(sender, receiver)
+   def initialize(sender, receiver, amount=50)
    @sender = sender
-    @receiver = receiver 
-  end
+   @receiver = receiver 
+   @amount = amount
+   @status = "pending"
+  end 
+ 
+  def status 
+   @status
+  end 
+  
+  def amount 
+    @amount 
+  end 
+  
+  def valid?
+    if @status == "open" && @amount > 0
+    return true
+    else 
+    return false 
+    binding.pry 
+    end
+    
+    if valid? && bank_account.valid?
+    else 
+     puts "there is a problem"
+    end 
+  end 
+  
+   
+  def execute_transaction
+  end 
+  
 end
