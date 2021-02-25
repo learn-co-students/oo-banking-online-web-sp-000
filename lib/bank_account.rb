ae1ae6057785c83bab7c4d32f7ded1782c4eb6dc
minirachel <rachel.a.levinson@gmail.com>
2021-02-25 17:41:42 -0500

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,36 @@
+require 'pry'
+require_relative './transfer.rb'
+
 class BankAccount
+
+    attr_accessor :balance, :status, :amount
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+
+    def deposit(amount)
+        @balance += amount
+    end
+
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+
+    def valid?
+        #balance above 0
+        #status is open
+
+
+        balance > 0 && status == "open"
+    end
+
+    def close_account
+        @status = "closed"
+    end
+
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,30 @@
+require 'pry'
+require_relative './bank_account.rb'
+
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+#      Failure/Error: let(:transfer) { Transfer.new(amanda, avi, 50) }
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
 end
+
+# a space for a transaction between two instances of the bank account class. 
+# you can't just transfer money to another account without the bank running checks first. 
+
+#reject a transfer IF
+# accounts are invalid
+# sender does not have money
+
+# pending >
+#     complete > reversed
+#     rejected
