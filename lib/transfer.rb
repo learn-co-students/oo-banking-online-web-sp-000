23046c6cb54eb976822ad0b23ca09d590a275eec
minirachel <rachel.a.levinson@gmail.com>
2021-02-25 18:07:26 -0500

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,6 +16,29 @@ class Transfer
   def valid?
     sender.valid? && receiver.valid?
   end
+  
+  def execute_transaction
+    if valid? && @status != "complete" && @sender.balance >= @amount
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if valid? && @status == "complete" && @receiver.balance >= @amount
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
 end
 
 # a space for a transaction between two instances of the bank account class. 
