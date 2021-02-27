69eaf3b444156a81457e8adf86bfdb6c8e189c07
HELEN HARMAN <hlharman@bellsouth.net>
2021-02-27 02:41:08 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -19,7 +19,7 @@ class BankAccount
   end 
 
   def display_balance
-   return "Your balance is $#{@balance}."
+   return "Your balance is $#{@balance}." 
   end
 
 
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,7 +10,7 @@ class Transfer
    @status = "pending"
   end 
  
-  def status 
+  def status
    @status
   end 
   
@@ -29,12 +29,23 @@ class Transfer
   
    
   def execute_transaction
-    Transfer.new(sender, receiver, amount)
-    # pry(#<Transfer>)> @name => nil
+    if sender.balance > amount && valid? && self.status == "pending" 
+      sender.balance = sender.balance - amount  
+     receiver.balance = receiver.balance + amount 
+     @status = "complete"
+  else 
+     @status = "rejected"
+    "Transaction rejected. Please check your account balance." 
+  end 
+end 
+end   
+ 
+  # "each transfer can only happen once" 
+  # rejects a transfer if the sender does not have enough funds (does not have a valid account)" do
+  #     "Transaction rejected. Please check your account balance."
+
+# pry(#<Transfer>)> @name => nil
     # pry(#<Transfer>)> @amount => 50
     # pry(#<Transfer>)> BankAccount.name => "BankAccount"
     # expect(amanda.balance).to eq(950)
     #   expect(avi.balance).to eq(1050)
-  end 
-  
-end
