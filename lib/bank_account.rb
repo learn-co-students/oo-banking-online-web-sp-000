0957359823a6ce32568b36955f7dff1bd0e2a3ab
Tim Quimbo <tquimbo1@gmail.com>
2021-03-15 17:31:31 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,36 @@
 class BankAccount
 
+  attr_accessor :name, :balance, :status
+
+  def initialize(name)
+    @name = name
+    account
+  end
+
+  def account
+    @balance = 1000
+    @status = 'open'
+  end
+
+
+  def deposit(money)
+    @balance += money
+  end
+
+  def display_balance
+    return "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if @status == 'open' && @balance > 0
+      return true
+    else
+      return false
+    end
+  end
+
+  def close_account
+    return @status = 'closed'
+  end
+
 end
--- lib/transfer.rb
+++
@@ -1,3 +0,0 @@
-class Transfer
-  # your code here
-end
