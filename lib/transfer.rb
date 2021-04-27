bb50e1a753d25f7c82d73b10f871f0d7b48d93f1
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-27 23:35:02 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,34 @@
+require "pry"
+
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status 
+  
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount 
+    @status = "pending"
+  end 
+  
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end 
+  
+  def execute_transaction
+    if @sender.balance > @amount 
+      @sender.deposit(0-@amount)
+      @receiver.deposit(@amount)
+      @amount = 0
+      @status = "complete"
+    else 
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end 
+  end 
+  
+  def reverse_transfer
+      
+  end
+  
 end
