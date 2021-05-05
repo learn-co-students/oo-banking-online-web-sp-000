3d0b9e5d65daaee5c50064981b90c8e0e77f8d50
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-05-05 17:26:05 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,13 +10,13 @@ class Transfer
   end 
   
   def valid?
-    if sender.valid? && receiver.valid? 
+    if @sender.valid? && @receiver.valid? 
       true 
     end
   end 
   
   def execute_transaction
-    if valid? && sender.balance > @amount && @status == "pending"
+    if valid? && @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
