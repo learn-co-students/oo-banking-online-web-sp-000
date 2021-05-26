0b097c95eb9b70a4825c28f4c7d9659b197d9f40
dotnotation <dotinmotionproductions@gmail.com>
2021-05-26 13:54:06 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
+require 'pry'
 class BankAccount
+    # one instance of the class can transfer money to another instance through Transfer class
+    # initialize with a name, balance of 1000, status open, can't change name
+    attr_accessor :status, :balance
+    attr_reader :name
+    
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+
+    def deposit(amount) 
+        @balance = @balance + amount
+    end
+
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+
+    def valid?
+        @status == "open" && @balance > 0
+    end
+    
+    def close_account
+        @status = "closed"
+    end
 
 end
