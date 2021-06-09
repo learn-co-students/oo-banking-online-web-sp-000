1067e8104e639f3666bb85b31f9319362eb3218d
ryan whitney <whitney.ryan.a@gmail.com>
2021-06-09 03:51:38 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
+  attr_accessor :name, :balance, :status
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(cashdrop)
+    @balance += cashdrop
+    @balance
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if @balance > 0 && @status == "open"
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    @status = "closed"
+  end
 
 end
