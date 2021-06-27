34c2b3b70886719e2c1137508f21efbdcfb65138
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2021-06-27 20:51:18 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
-
+  attr_accessor :balance, :status
+  attr_reader :name #can't change its name
+  
+  def initialize(bank_account) #can initialize a bank account
+    @name = bank_account #initializes with a name
+    @balance= 1000 #always initializes with a balance of 1000
+    @status = "open" #always initializes with a status of open
+  end
+  
+  def deposit(cash) #can deposit money into its account
+    @balance += cash
+  end
+  
+  def display_balance #can display its balance
+    "Your balance is $#{@balance}."
+  end
+  
+  def valid? #is valid with an open status and balance greater than 0
+    if @status == "open" && @balance > 0 
+      true 
+    else 
+      false 
+    end
+  end
+  
+  def close_account #can close its account
+    @status = "closed"
+  end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,38 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount) #can initialize a transfer
+    @sender = sender #initializes with a sender
+    @receiver = receiver #initializes with a recceiver
+    @amount = amount #initializes with a transfer amount
+    @status = "pending" #always initializes with a status of pending
+  end
+  
+  def valid? #can check that both accounts are valid
+    if sender.valid? && receiver.valid? #calls on the sender and receiver valid? methods
+      true 
+    else 
+      false 
+    end
+  end
+  
+  def execute_transaction #can exdecute a successful transaction between two accounts
+    if @sender.balance >= @amount && @status == "pending" && valid? == true
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete" #each transfer can only happen once
+    else 
+      @status = "rejected" #rejects a transfer if the sender does not have enough funds (does not have a valid account)
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+  
+  def reverse_transfer #can reverse a transfer between two accounts
+    if @status == "complete" #it can only reverse executed transfers
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
+  end
 end
